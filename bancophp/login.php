2b29143eca8557f529c8e0aa77e81a9e5c499acb
emanuel074 <emanuelalmeida074@gmail.com>
2016-05-23 15:12:08 -0300

    login banco php

---
+++ bancophp/login.php
@@ -0,0 +1,20 @@
+﻿<?php
+   $usuario =$_POST['usuario'];
+   $senha =$_POST['senha'];
+    
+    $conexao = mysql_connect('localhost','root','');
+   
+   mysql_select_db('bancoDengue',$conexao);
+      
+   $sql="select * from login where usuario like '$usuario' and senha like '$senha'";
+  
+   $resultado = mysql_query($sql) or die ("Erro: " . mysql_error());
+
+  while($linha = mysql_fetch_object($resultado)) {
+   echo $linha->usuario."#";
+   echo $linha->senha."^";
+      }
+
+echo "^";
+
+?>
